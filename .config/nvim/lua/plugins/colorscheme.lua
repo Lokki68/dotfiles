12d8529f6a7c33083eaa591f085b043ac7b7a5c0
Brice <b.libert@ideematic.com>
2025-04-16 09:27:40 +0200

    fix colorscheme nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,20 @@
 return {
-  "EdenEast/nightfox.nvim",
-  version = false,
+  "craftzdog/solarized-osaka.nvim",
   lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  -- Optional; default configuration will be used if setup isn't called.
-  config = function()
-    require("nightfox").setup({
-      -- Your config here
-      options = {
-        transparent = true,
-        palettes = { "nordfox" },
-      },
-    })
-  end,
+  priority = 1000,
+  opts = {
+    transparent = true,
+    terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+    styles = {
+      -- Style to be applied to different syntax groups
+      -- Value is any valid attr-list value for `:help nvim_set_hl`
+      comments = { italic = true },
+      keywords = { italic = true },
+      functions = {},
+      variables = {},
+      -- Background styles. Can be "dark", "transparent" or "normal"
+      sidebars = "dark", -- style for sidebars, see below
+      floats = "dark", -- style for floating windows
+    },
+  },
 }
